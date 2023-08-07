bb865e9ae1bd0454226773fcc099e607f8b48c68
William11Cassidy <121349385+William11Cassidy@users.noreply.github.com>
2023-08-07 09:48:23 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -8,6 +8,36 @@ function modelReady() {
     classifier.classify(gotResult)
 }
 
-function gotResult() {
-    console.log("Result");
+function gotResult(error, result) {
+    if (error) {
+        console.error(error);
+    } else {
+        document.getElementById("rusultlabel").innerHTML = "i can hear " + result[0].label
+        document.getElementById("resultacurracy").innerHTML = "confidence " + (result[0].confidence * 100).toFixed(2) + " %"
+        console.log(result);
+
+
+        if (result[0].label == 'goat') {
+            document.getElementById("img").src = "goat.jpeg"
+        } else if (result[0].label == 'dog') {
+            document.getElementById("img").src = "dog.jpeg"
+        }
+        else if (result[0].label == 'aligator') {
+            document.getElementById("img").src = "aligator.jpeg"
+        }
+        else if (result[0].label == 'cow') {
+            document.getElementById("img").src = "cow.jpeg"
+        } else {
+            document.getElementById("img").src = "soundicon.jpeg"
+
+        }
+
+
+    }
+
+
+
 }
+
+
+
